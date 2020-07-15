29ae265180344d65d70ff109850d32a0699477a1
dm.perlov <dm.perlov@jet.su>
2020-07-15 15:07:10 +0300

    initial set-up

---
+++ app/app.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import styles from "./app.css";
+
+export const App = () => <div className={styles.helloWorld}> Hello world </div>;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+module.exports = {
+  context: __dirname,
+  entry: "./app/index.jsx",
+  output: {
+    path: __dirname + "/dist/",
+    filename: "bundle.js"
+  },
+  devtool: "#sourcemap",
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { modules: true } }
+        ]
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    modules: ["app", "node_modules"],
+    extensions: [".js", ".jsx"]
+  }
+};
