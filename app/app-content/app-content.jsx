45549d66fa842a2659ea8ec8cf1d3ca601728adc
dm.perlov <dm.perlov@jet.su>
2020-07-15 16:23:24 +0300

    content navbar refactoring

--- app/app-content/app-content.jsx
+++ app/app-content/app-content.jsx
@@ -1,37 +1,35 @@
 import React from "react";
+import { Link, Switch, Route, useRouteMatch } from "react-router-dom";
 import styles from "./app-contents.css";
 
-console.log({ styles });
 
-export const AppContent = () => (
-  <div className={styles.container}>
-    <div className={styles.header}>
-      <h2>метки</h2>
+const navBarItems = [
+  { name: "вращение", path: "rotation" },
+  { name: "отведение", path: "abduction" },
+  { name: "приведение", path: "cast" },
+  { name: "пронация", path: "pronation" },
+  { name: "разгибание", path: "extension" },
+  { name: "сгибание", path: "bending" },
+  { name: "супинация", path: "supination" }
+];
+
+export const AppContent = () => {
+  let match = useRouteMatch();
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h2>метки</h2>
+      </div>
+      <div className={styles.content}>
+        <ul>
+          {navBarItems.map(({ name, path }) => (
+            <li key={path}>
+              <a href={`#${path}`}>{name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-    <div className={styles.content}>
-      <ul>
-        <li>
-          <a href="#0">вращение</a>
-        </li>
-        <li>
-          <a href="#1">отведение</a>
-        </li>
-        <li>
-          <a href="#2">приведение</a>
-        </li>
-        <li>
-          <a href="#3">пронация</a>
-        </li>
-        <li>
-          <a href="#4">разгибание</a>
-        </li>
-        <li>
-          <a href="#4">сгибание</a>
-        </li>
-        <li>
-          <a href="#5">супинация</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-);
+  );
+};
